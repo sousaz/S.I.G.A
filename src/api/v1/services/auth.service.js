9720ee313ccad8d590145b40e1cb9b3972a52133
sousaz <gabrielsousa2003gsa@gmail.com>
2024-05-24 11:31:01 -0300

    fix: API - Login

--- src/api/v1/services/auth.service.js
+++ src/api/v1/services/auth.service.js
@@ -7,7 +7,7 @@ module.exports = {
         const user = await userService.getUserByUsername(username);
         if(!user)
             return null;
-        const checkPass = brcypt.compare(password, user.password)
+        const checkPass = await brcypt.compare(password, user.password)
         if(!user || !checkPass)
             return null;
         return { id: user._id, username: user.username, role: user.role }
