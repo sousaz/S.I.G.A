c6e422d2da30bcfd3faad6a6f49b3f018b92e7e9
Suellen Oliveira <susu5669@gmail.com>
2024-06-19 18:50:37 -0300

    swagger login pronto

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 const app = require("./app")
 const connect = require("./src/config/mongo")
+const swaggerUi = require("swagger-ui-express")
+const swaggerDocs = require("./swagger.json")
 
 const port = process.env.PORT || 3000
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 const init = app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 connect(init)
+
+
+
 
 
