54c31d33fc2de3a359fb643bed8a9b2d7850e21f
sousaz <gabrielsousa2003gsa.gmail.com>
2024-06-27 19:42:40 -0300

    refactor: Update delete method in degp.route.js to use req.params.id instead of req.body.id

--- backend/src/api/v1/routes/degp.route.js
+++ backend/src/api/v1/routes/degp.route.js
@@ -4,9 +4,10 @@ const tokenMiddleware = require('../middleware/token.middleware');
 
 router.route("/degp")
     .post(tokenMiddleware.verifyToken, degpController.create)
-    .delete(tokenMiddleware.verifyToken, degpController.delete)
     .put(tokenMiddleware.verifyToken, degpController.update)
-    .get(tokenMiddleware.verifyToken, degpController.list);
+    .get(tokenMiddleware.verifyToken, degpController.list)
+
+router.route("/degp/:id").delete(tokenMiddleware.verifyToken, degpController.delete)
 
 router.route("/degp/access").get(tokenMiddleware.verifyToken, degpController.listAccess)
 
